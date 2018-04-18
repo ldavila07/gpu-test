d2c06abbc1203f053bb5c12b2835738d65976805
Luis Davila <ldavila07@icloud.com>
2018-04-18 16:47:01 -0500

    uncommented wget commands

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#wget http://vision.stanford.edu/aditya86/ImageNetDogs/images.tar
-#tar -xvf Images.tar
-#wget http://vision.stanford.edu/aditya86/ImageNetDogs/annotation.tar
-#tar -xvf Annotation.tar
+wget http://vision.stanford.edu/aditya86/ImageNetDogs/images.tar
+tar -xvf images.tar
+wget http://vision.stanford.edu/aditya86/ImageNetDogs/annotation.tar
+tar -xvf annotation.tar
 python gen-images.py  ./Images/ ./Annotation/
